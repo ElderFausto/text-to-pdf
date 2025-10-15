54103cc7709e3b2eba88e2ac60987dcfa63bb263
elderfausto <elderfavsto@gmail.com>
2025-10-15 11:43:50 -0300

    chore: project setup

---
+++ src/main/java/com/itext/text_to_pdf/TextToPdfApplication.java
@@ -0,0 +1,13 @@
+package com.itext.text_to_pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextToPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextToPdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itext/text_to_pdf/TextToPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itext.text_to_pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextToPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
